2dd99f8c5b92313c2c7586f53ccd5017ac910301
ehrrrr <elisaveta.s.hristova@gmail.com>
2019-07-16 22:14:28 +0300

    Updates

---
+++ precache-manifest.0a643d2d24360007e5f420c97c4b9be5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d77024d66a82e63e478f03f8bd542ad0",
+    "url": "/ehrrrr/star-wars-app/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/ehrrrr/star-wars-app/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "726ae6729ddc5428e02e",
+    "url": "/ehrrrr/star-wars-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/ehrrrr/star-wars-app/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "726ae6729ddc5428e02e",
+    "url": "/ehrrrr/star-wars-app/static/js/main.a540eb5d.chunk.js"
+  },
+  {
+    "revision": "6c5a6faaf2726fb1dac0",
+    "url": "/ehrrrr/star-wars-app/static/js/runtime~main.e7b7effd.js"
+  }
+]);
